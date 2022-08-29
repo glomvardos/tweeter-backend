758fef308f5d4112f61309a38116edd6e88c5e4c
glomvardos <glomvardos@gmail.com>
2022-08-29 17:58:31 +0300

    Added unsave and get saved tweets endpoints

--- src/saved-tweets/saved-tweets.controller.ts
+++ src/saved-tweets/saved-tweets.controller.ts
@@ -1,16 +1,38 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { GetUser } from 'src/auth/decorator/get-user.decorator';
 import { JwtGuard } from 'src/auth/guard/jwt.guard';
 import { SaveTweetDto } from './dto/dto';
 import { SavedTweetsService } from './saved-tweets.service';
 
 @UseGuards(JwtGuard)
-@Controller('saved-tweets')
+@Controller('api/saved-tweets')
 export class SavedTweetsController {
   constructor(private savedTweetsService: SavedTweetsService) {}
+
+  @Get('save-tweet')
+  getSavedTweets(@GetUser('id') userId: number) {
+    return this.savedTweetsService.getSavedTweets(userId);
+  }
 
   @Post('save-tweet')
   saveTweet(@GetUser('id') userId: number, @Body() dto: SaveTweetDto) {
     return this.savedTweetsService.saveTweet(userId, dto);
+  }
+
+  @Delete('save-tweet/:savedTweetId')
+  unSaveTweet(
+    @GetUser('id') userId: number,
+    @Param('savedTweetId', ParseIntPipe) savedTweetId: number,
+  ) {
+    return this.savedTweetsService.unSaveTweet(userId, savedTweetId);
   }
 }
--- src/saved-tweets/saved-tweets.service.ts
+++ src/saved-tweets/saved-tweets.service.ts
@@ -6,6 +6,70 @@ import { SaveTweetDto } from './dto/dto';
 export class SavedTweetsService {
   constructor(private prisma: PrismaService) {}
 
+  async getSavedTweets(userId: number) {
+    const savedTweets = await this.prisma.savedTweet.findMany({
+      where: {
+        userId,
+      },
+      include: {
+        tweet: {
+          select: {
+            id: true,
+            createdAt: true,
+            updatedAt: true,
+            description: true,
+            isPublic: true,
+            userId: true,
+            user: {
+              select: {
+                firstname: true,
+                lastname: true,
+              },
+            },
+            likes: {
+              where: {
+                userId,
+              },
+            },
+            comments: {
+              select: {
+                id: true,
+                createdAt: true,
+                description: true,
+                user: {
+                  select: {
+                    firstname: true,
+                    lastname: true,
+                  },
+                },
+                _count: {
+                  select: {
+                    likes: true,
+                  },
+                },
+                likes: {
+                  where: {
+                    userId,
+                  },
+                },
+              },
+            },
+            savedTweets: {
+              where: {
+                userId,
+              },
+            },
+          },
+        },
+      },
+    });
+    const returnedTweets = savedTweets.map((tweet) => ({
+      ...tweet.tweet,
+    }));
+
+    return returnedTweets;
+  }
+
   async saveTweet(userId: number, dto: SaveTweetDto) {
     const alreadySaved = await this.prisma.savedTweet.findFirst({
       where: {
@@ -26,6 +90,27 @@ export class SavedTweetsService {
         tweet: {
           connect: { id: dto.tweetId },
         },
+      },
+    });
+
+    return savedTweet;
+  }
+
+  async unSaveTweet(userId: number, savedTweetId: number) {
+    const alreadySaved = await this.prisma.savedTweet.findFirst({
+      where: {
+        id: savedTweetId,
+        userId,
+      },
+    });
+
+    if (!alreadySaved) {
+      throw new BadRequestException('You did not saved this tweet');
+    }
+
+    const savedTweet = await this.prisma.savedTweet.delete({
+      where: {
+        id: savedTweetId,
       },
     });
 
